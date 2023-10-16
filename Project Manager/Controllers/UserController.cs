32644e7c1587826dcad765f9893197e9cb1b58bc
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-10-16 15:07:58 +0200

    created sign up and sign in pages

---
+++ Project Manager/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Manager.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
