57f804a1bf51bc17ad697a99642a60b3a1861e73
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-10-17 15:09:36 +0200

    added project views

---
+++ Project Manager/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Manager.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateProject()
+        {
+            return View();
+        }
+
+        public IActionResult UpdateProject()
+        {
+            return View();
+        }
+    }
+}
