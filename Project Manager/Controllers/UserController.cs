5f95f673de64c2d416d57ce59cbb2ab6ea026f62
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-10-19 11:27:50 +0200

    can now register user

--- Project Manager/Controllers/UserController.cs
+++ Project Manager/Controllers/UserController.cs
@@ -1,22 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_Manager.Models;
 
 namespace Project_Manager.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult SignIn()
         {
-            return View();
+            var db = new project_manager_dbContext();
+
+            var model = db.User.ToList();
+
+
+            return View(model);
         }
 
         public IActionResult SignUp()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUser(User formData)
+        {
+            //TODO check if all fields were filled
+            //TODO check if the user already exist
+            //TODO show errors to user
+            //TODO show success
+            var user = new User();
+            var db = new project_manager_dbContext();
+
+            user.Name = formData.Name;
+            user.Email = formData.Email;
+            user.Password = formData.Password;
+
+            db.User.Add(user);
+            db.SaveChanges();
+
+            return RedirectToAction("SignIn");
         }
     }
 }
