bfc240423098777e8f258fcf2dd3d071ccb54bdf
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-10-26 11:42:15 +0200

    can now delete all elements in materials list

--- Project Manager/wwwroot/js/createProject.js
+++ Project Manager/wwwroot/js/createProject.js
@@ -1,24 +1,79 @@
-const materialUl = document.querySelector(".materials-list");
+const materialUl = document.querySelector("ul");
 const addMaterialButton = document.querySelector(".add-material-button");
 const listItems = document.querySelectorAll(".materials-list li");
 const checkboxes = document.querySelectorAll("li [type=checkbox]");
 
 let materialIndex = 0;
+let idIndex = 0;
 
 addMaterialButton.addEventListener("click", () => {
-    console.log("button clicked");
+    //console.log("button clicked");
 
     CreateListItem();
 
-    materialIndex++;
+    idIndex++;
 });
+
+const DeleteMaterial = (event) => {
+    //console.log(event.target.id);    
+    var button = document.getElementById(event.id);
+
+    //console.log(materialUl);
+
+    for (var i = 0; i < materialUl.children.length; i++) {
+        //console.log(i);
+        //console.log(materialUl.children[i].id);
+        //console.log(event.target.id);
+
+
+        if (materialUl.children[i].id == `list-${event.target.id}`) {
+            //console.log("match");
+            var listItem = document.getElementById(`list-${event.target.id}`);
+            //console.log(listItem);
+
+            materialUl.removeChild(listItem);
+
+            /*if (materialUl.length == 1) {
+                materialUl.removeChild(materialUl.firstChild);
+            }*/
+        } else {
+            //console.log("no match");
+        }
+    }
+    //console.log(listItem);
+
+
+    // all list items in an array
+    let array = [...materialUl.children];
+
+    //console.log(array);
+
+    // printing all list items inputs
+    //idIndex = 0;
+
+    array.forEach((element) => {
+
+
+        //console.log(element.id);
+        //element.id = `list-${idIndex}`;
+        //idIndex++;
+
+        for (let i = 0; i < element.children.length; i++) {
+            //console.log(element.children[i]);
+            //console.log(element.children[i].name);            
+
+
+            //element.children[i].name = `Material[${materialIndex}].Amount`;
+        }
+    });
+}
 
 const CheckCheckBox = (event) => {
     if (event.target.checked) {
-        console.log("checked");
+        //console.log("checked");
         event.target.value = true;
     } else {
-        console.log("not checked");
+        //console.log("not checked");
         event.target.value = false;
     }
 }
@@ -26,12 +81,14 @@ const CheckCheckBox = (event) => {
 const CreateListItem = () => {
 
     const materialContainer = document.createElement("li");
-    materialContainer.id = materialIndex;
+    materialContainer.id = `list-${idIndex}`;
     materialContainer.classList.add("material-container");
 
     const materialNameInput = document.createElement("input");
     const materialAmountInput = document.createElement("input");
     const materialAcquiredInput = document.createElement("input");
+    const deleteMaterialInput = document.createElement("div");
+    const deleteMaterialTextbox = document.createElement("p");
 
     materialNameInput.type = "text";
     materialNameInput.name = `Material[${materialIndex}].Name`;
@@ -44,27 +101,19 @@ const CreateListItem = () => {
     materialAcquiredInput.value = false;
     materialAcquiredInput.addEventListener('change', CheckCheckBox);
 
+    deleteMaterialInput.name = `Material[${materialIndex}].Acquired`;
+    deleteMaterialInput.id = `${idIndex}`
+    deleteMaterialInput.addEventListener('click', DeleteMaterial);
+
+    deleteMaterialTextbox.textContent = "Delete";
+    deleteMaterialTextbox.id = `${idIndex}`
+
     materialContainer.appendChild(materialNameInput);
     materialContainer.appendChild(materialAmountInput);
     materialContainer.appendChild(materialAcquiredInput);
+    materialContainer.appendChild(deleteMaterialInput);
+    deleteMaterialInput.appendChild(deleteMaterialTextbox);
+
     materialUl.appendChild(materialContainer);
-
-    // all list items in an array
-    let array = [...materialUl.children];
-    let childrenArray = [];
-
-    console.log(array);
-
-    // printing all list items inputs
-
-    array.forEach((element) => {
-        for (let i = 0; i < element.children.length; i++) {
-            console.log(element.children[i]);
-            console.log(element.children[i].name);
-            //element.children.setAttribute("Name", `Material[8].Amount`);
-            element.children[i].name = "Material[8].Amount";
-            //("ul > li").classList.add(("Material[8].Amount"));
-        }
-    });
 }
 
